ccf489d12fba480f83b5ed8f6683498a6e1714cb
Mecon0710 <m.contrerasr@uniandes.edu.co>
2023-02-27 21:01:28 -0500

    Funcionaaaaa

--- Assets/Scripts/animals.cs
+++ Assets/Scripts/animals.cs
@@ -10,7 +10,8 @@ public class animals : MovingEntity
     float wanderRadius = 2f;
     
     void RecalculateTargetPosition(){
-        targetPosition = transform.position + RandomPointInBounds()*wanderRadius;
+        
+        targetPosition = transform.position + RandomPointInBounds(); 
     }
 
     void Start()
@@ -20,15 +21,22 @@ public class animals : MovingEntity
 
     void Update()
     {
+        
         //var timer = new Timer(5000);
         towardsTarget = targetPosition - transform.position;
         MoveTowards(towardsTarget.normalized);
-        if (towardsTarget.y < 330 && towardsTarget.y > 328)
-            if (towardsTarget.x < -76 && towardsTarget.x > -92)
-                if (towardsTarget.z < -64 && towardsTarget.z > -50) 
-                        //timer.Interval = 5000;
+        if(towardsTarget.z %0.4 ==0 || towardsTarget.y %0.1 ==0 || towardsTarget.x %0.3 ==0
+        ){
                         RecalculateTargetPosition();
-                        //timer.Start();
+                        Debug.Log("y"+towardsTarget.y);
+                        Debug.Log("x"+towardsTarget.x);
+                        Debug.Log("z"+towardsTarget.z);
+        }
+        // if (towardsTarget.y < 172 && towardsTarget.y > 0)
+
+        //     if (towardsTarget.x < 20 && towardsTarget.x > -60)
+        //         if (towardsTarget.z < 10 && towardsTarget.z > -5) 
+        //                 //timer.Start();
         
         Debug.DrawLine(transform.position, targetPosition, Color.green );
     }
@@ -38,12 +46,12 @@ public class animals : MovingEntity
         // int x = rnd.Next(-92, -76);
         // int y = rnd.Next(328, 330);
         // int z = rnd.Next(-64,-50);
-        int x = rnd.Next(-34, -30);
-        int y = rnd.Next(0, 1);
-        int z = rnd.Next(-10,-10);
-        Debug.Log("x"+x);
-        Debug.Log("y"+y);
-        Debug.Log("z"+z);
+        int x = rnd.Next(1, 5);
+        int y = rnd.Next(1, 5);
+        int z = rnd.Next(1, 5);
+        // Debug.Log("x"+x);
+        // Debug.Log("y"+y);
+        // Debug.Log("z"+z);
         return new Vector3(x, y, z);
     }
 }
